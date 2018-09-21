726b9b5f4febd5c9be5b19a71a9d89490bfdd9a8
amitGroomefy <amit@groomefy.com>
2018-09-21 17:12:30 +0530

    completed readme

--- amit-readme.py
+++ amit-readme.py
@@ -1,3 +1,4 @@
 #for inferencing folder images and save them to other folder with filter fields
 #according to bounding box percentage>3% and single face detected
+
 python custom_inference_folder.py
